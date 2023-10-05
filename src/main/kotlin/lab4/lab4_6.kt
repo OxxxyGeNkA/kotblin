2d7857dfa40d69e3adc4e97893a376a8141987ed
OxxxyGeN <valeratoltek@gmail.com>
2023-10-06 02:10:20 +0300

    lab4

---
+++ src/main/kotlin/lab4/lab4_6.kt
@@ -0,0 +1,17 @@
+package lab4
+
+class DVDPlayer(var hasRecorder: Boolean) {
+    fun playDVD() {
+        println("DVD playing")
+    }
+    fun recordDVD() {
+        if (hasRecorder) {
+            println ("DVD recording")
+        }
+    }
+}
+fun main() {
+    val d = DVDPlayer(true)
+    d.playDVD()
+    d.recordDVD()
+}
